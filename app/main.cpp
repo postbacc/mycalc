6cfbc22c45db5bb333d733f4b0e89190d0d3d4bf
siag7072 <siag7072@colorado.edu>
2019-05-17 20:08:00 +0000

    added add and removed backup updated CMake

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+#include "../code/mycalc.h"
+#include <iostream>
+
+using namespace std;
+int main(){
+    int var;
+    cout << "Hello World, please enter a number " << endl;
+    cin >> var;
+    cout << "your number was : " << var << endl;
+
+    return 0;
+}
