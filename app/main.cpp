4cc2a70e1d04ac0268fd820bd0d7f75575b98e54
siag7072 <siag7072@colorado.edu>
2019-05-17 23:19:19 +0000

    added grading system to test

--- app/main.cpp
+++ app/main.cpp
@@ -2,11 +2,18 @@
 #include <iostream>
 
 using namespace std;
+
 int main(){
     int var;
     cout << "Hello World, please enter a number " << endl;
     cin >> var;
     cout << "your number was : " << var << endl;
 
+    mycalc o1;
+    o1.sub(var,2);
+    
+    mycalc o2;
+    o2.sum(var,var);
+    
     return 0;
 }
--- tests/test_mycalc.cpp
+++ tests/test_mycalc.cpp
@@ -1,25 +1,50 @@
 #include <gtest/gtest.h>
+#include <fstream>
+#include <iostream>
 #include "../code/mycalc.h"
 
 class mycalcTest : public ::testing::Test {
 protected:
-	void SetUp() override {
+	static void SetUpTestCase(){
 	}
-	//void TearDown() override {}
+	static void TearDownTestCase(){
+		std::ofstream outgrade("total_grade.txt");
+		if(outgrade.is_open())
+		outgrade.clear();
+
+		outgrade << total_grade;
+		outgrade.close();
+	}
+
+	void add_points_to_grade(int points){
+		if(!::testing::Test::HasFailure()){
+			total_grade += points;
+		}
+	}
+
+	void SetUp() override {}
+
+	void TearDown() override {}
 	
 	mycalc mycalcobj;
+	static int total_grade;
 };
+
+int mycalcTest::total_grade = 0;
 
 TEST_F(mycalcTest, SumFuncForPositiveResult){
 	EXPECT_EQ(5,mycalcobj.sum(2,3));
+	add_points_to_grade(10);
 }
 
 TEST_F(mycalcTest, SumFuncForNegativeResult){
 	EXPECT_EQ(-1,mycalcobj.sum(-3,2));
+	add_points_to_grade(5);
 }
 
 TEST_F(mycalcTest, SubFunc){
 	EXPECT_EQ(1,mycalcobj.sub(3,2));
+	add_points_to_grade(2);
 }
 
 
